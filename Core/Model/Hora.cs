80113f8f0cd5cf5491645ab76103a4fd066cddd2
Miriam <mirianmc1998@gmail.com>
2022-03-16 13:52:28 +0100

    añadir propiedad

--- Core/Model/Hora.cs
+++ Core/Model/Hora.cs
@@ -3,6 +3,7 @@ public class Hora
     public int ID { get; set; }
 
     public  TimeSpan HoraInicio { get; set; }
-     public  TimeSpan HoraFin { get; set; }
-     
+    public  TimeSpan HoraFin { get; set; }
+
+    public DiaSemana DiasSemana { get; set;}
 }
