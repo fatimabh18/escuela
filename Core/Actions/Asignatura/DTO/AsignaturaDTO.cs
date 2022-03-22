a34d6fdecbd14b5e0aeae5513fee9d128206145f
Fatima <fatimabha99@gmail.com>
2022-03-22 09:51:47 +0100

    asignaturaViewModelyDTO

---
+++ Core/Actions/Asignatura/DTO/AsignaturaDTO.cs
@@ -0,0 +1,6 @@
+﻿public class AsignaturaDTO
+{
+    public int id { get; set; }
+
+    public string nombre { get; set; }
+}
---
+++ Core/Actions/Asignatura/ViewModel/AsignaturaViewModel.cs
@@ -0,0 +1,4 @@
+﻿public class AsignaturaViewModel
+{
+    public string Nombre { get; set; }
+}
