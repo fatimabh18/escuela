924b32abb0ab6de7c95eff70250eadbe2af14239
Miriam <mirianmc1998@gmail.com>
2022-03-29 10:16:04 +0200

    dto y view model hecho

---
+++ Core/Actions/Escuela/DTO/DTO.cs
@@ -0,0 +1,6 @@
+public class EscuelaDTO
+{
+    public int id { get; set; }
+
+    public string nombre { get; set; }
+}
--- Core/Model/Alumno.cs
+++ Core/Model/Alumno.cs
@@ -2,4 +2,4 @@ public class Alumno
 {
     public int ID { get; set;}
     public string nombre {get; set;}
-}
+} 
