a24ed761b711f101d07b96d426ccbd0133776955
merienditas <maricersal@hotmail.com>
2022-03-29 11:07:47 +0200

    metodos creados alumno

--- Core/Model/Alumno.cs
+++ Core/Model/Alumno.cs
@@ -1,5 +1,12 @@
 public class Alumno
 {
-    public int id { get; set;}
-    public string nombre {get; set;}
+    public int Id { get; set;}
+    public string Nombre {get; set;}
+
+    public Alumno (int id, string nombre)
+    {
+        Id = id;
+        Nombre = nombre;
+    }
+
 } 
